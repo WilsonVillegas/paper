c238d2908155f280db10483f8be86e33d95a1a75
Wilson <wilsonvillegas@rocketmail.com>
2014-04-06 19:24:49 -0500

    Overhaul, implemented John's model and added primitives for furniture

--- Paper/Assets/Scripts/PaperMotor.cs
+++ Paper/Assets/Scripts/PaperMotor.cs
@@ -5,21 +5,30 @@ public class PaperMotor : MonoBehaviour {
 
 	public float speed;
 	public bool hit = false;
+	public bool started = false;
 	public int lives;
 	float OriginalSpeed;
+	float DoubleSpeed;
+	float HalfSpeed;
+	Vector3 position;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		StartCoroutine(StartLevel());
 		OriginalSpeed = speed;
+		HalfSpeed = speed/4;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		position = transform.position;
 		//Quaternion wantedRotation = new Vector3(0,0,0);
-		if(!hit)
+		if(!hit && started)
 		{
 			if(Input.GetButton("Jump"))
-				speed = speed * 2;
+			{
+				speed = HalfSpeed;
+			}
 			else
 				speed = OriginalSpeed;
 
@@ -56,9 +65,17 @@ public class PaperMotor : MonoBehaviour {
 
 		lives--;
 
-		//Application.LoadLevel(2);
+		Application.LoadLevel(2);
 		//hit = true;
 	}
+
+		private IEnumerator StartLevel()
+		{
+			yield return new WaitForSeconds(2);
+			started = true;
+		}
+
+
 
 
 }
